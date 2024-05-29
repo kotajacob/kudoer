deed355b62c567f2897359bd7b056b451e28cb5d
Dakota Walsh <kota@nilsu.org>
2024-05-29 16:29:25 +1200

    Configurable http address

--- main.go
+++ main.go
@@ -3,19 +3,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":2024", "HTTP Network Address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /kudo/{id}", kudoView)
 	mux.HandleFunc("GET /kudo/create", kudoCreate)
 	mux.HandleFunc("POST /kudo/create", kudoCreatePost)
 
-	log.Println("listening on :2024")
+	log.Println("listening on", *addr)
 
-	err := http.ListenAndServe(":2024", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatalln(err)
 }
