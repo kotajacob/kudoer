752ed9ecc95ea17814049eb5b79693a2c6bbaa16
Dakota Walsh <kota@nilsu.org>
2024-06-24 15:29:43 +1200

    Introduce database migration system

--- db.go
+++ db.go
@@ -6,10 +6,75 @@ import (
 	"context"
 
 	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitemigration"
 	"zombiezen.com/go/sqlite/sqlitex"
 )
 
 func openDB(dsn string) (*sqlitex.Pool, error) {
+	schema := sqlitemigration.Schema{
+		// Each element of the Migrations slice is applied in sequence. When you
+		// want to change the schema, add a new SQL script to this list.
+		//
+		// Existing databases will pick up at the same position in the Migrations
+		// slice as they last left off.
+		Migrations: []string{
+			`CREATE TABLE IF NOT EXISTS users (
+			username TEXT NOT NULL PRIMARY KEY,
+			displayname TEXT NOT NULL,
+			email TEXT NOT NULL,
+			password TEXT NOT NULL
+		) WITHOUT ROWID;`,
+			`CREATE UNIQUE INDEX IF NOT EXISTS users_idx ON users (username);`,
+
+			`CREATE TABLE IF NOT EXISTS items (
+			id TEXT NOT NULL PRIMARY KEY,
+			creator_username TEXT NOT NULL,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL,
+			image TEXT NOT NULL,
+			FOREIGN KEY (creator_username) REFERENCES users (username)
+		) WITHOUT ROWID;`,
+			`CREATE UNIQUE INDEX IF NOT EXISTS items_idx ON items (id);`,
+
+			`CREATE VIRTUAL TABLE IF NOT EXISTS items_search USING fts5(
+			id,
+			name,
+			description
+		);`,
+			`CREATE TRIGGER IF NOT EXISTS after_items_insert AFTER INSERT ON items
+			BEGIN INSERT INTO items_search(
+				id,
+				name,
+				description
+			)
+			VALUES (
+				new.id,
+				new.name,
+				new.description
+			);
+		END;`,
+
+			`CREATE TABLE IF NOT EXISTS kudos (
+			id TEXT NOT NULL PRIMARY KEY,
+			item_id TEXT NOT NULL,
+			creator_username TEXT NOT NULL,
+			emoji INTEGER NOT NULL,
+			body TEXT NOT NULL,
+			FOREIGN KEY (creator_username) REFERENCES users (username)
+		) WITHOUT ROWID;`,
+			`CREATE UNIQUE INDEX IF NOT EXISTS kudos_idx ON kudos (id);`,
+			`CREATE INDEX IF NOT EXISTS kudos_item_idx ON kudos (item_id);`,
+			`CREATE INDEX IF NOT EXISTS kudos_creator_usernamex ON kudos (creator_username);`,
+
+			`CREATE TABLE IF NOT EXISTS sessions (
+			token TEXT PRIMARY KEY,
+			data BLOB NOT NULL,
+			expiry REAL NOT NULL
+		);`,
+			`CREATE INDEX IF NOT EXISTS sessions_expiry_idx ON sessions(expiry);`,
+		},
+	}
+
 	db, err := sqlitex.NewPool(dsn, sqlitex.PoolOptions{
 		PoolSize: 10,
 		PrepareConn: func(conn *sqlite.Conn) error {
@@ -26,160 +91,13 @@ func openDB(dsn string) (*sqlitex.Pool, error) {
 		return nil, err
 	}
 
+	// Run migrations.
 	conn, err := db.Take(context.Background())
 	if err != nil {
 		return nil, err
 	}
 	defer db.Put(conn)
-
-	// Create users table.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE TABLE IF NOT EXISTS users (
-			username TEXT NOT NULL PRIMARY KEY,
-			displayname TEXT NOT NULL,
-			email TEXT NOT NULL,
-			password TEXT NOT NULL
-		) WITHOUT ROWID;`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create users index.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE UNIQUE INDEX IF NOT EXISTS users_idx ON users (username);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create items table.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE TABLE IF NOT EXISTS items (
-			id TEXT NOT NULL PRIMARY KEY,
-			creator_username TEXT NOT NULL,
-			name TEXT NOT NULL,
-			description TEXT NOT NULL,
-			image TEXT NOT NULL,
-			FOREIGN KEY (creator_username) REFERENCES users (username)
-		) WITHOUT ROWID;`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create items index.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE UNIQUE INDEX IF NOT EXISTS items_idx ON items (id);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create items full text search.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE VIRTUAL TABLE IF NOT EXISTS items_search USING fts5(
-			id,
-			name,
-			description
-		);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-	err = sqlitex.Execute(
-		conn,
-		`CREATE TRIGGER IF NOT EXISTS after_items_insert AFTER INSERT ON items
-			BEGIN INSERT INTO items_search(
-				id,
-				name,
-				description
-			)
-			VALUES (
-				new.id,
-				new.name,
-				new.description
-			);
-		END;`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create kudos table.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE TABLE IF NOT EXISTS kudos (
-			id TEXT NOT NULL PRIMARY KEY,
-			item_id TEXT NOT NULL,
-			creator_username TEXT NOT NULL,
-			emoji INTEGER NOT NULL,
-			body TEXT NOT NULL,
-			FOREIGN KEY (creator_username) REFERENCES users (username)
-		) WITHOUT ROWID;`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create kudos index.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE UNIQUE INDEX IF NOT EXISTS kudos_idx ON kudos (id);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-	err = sqlitex.Execute(
-		conn,
-		`CREATE INDEX IF NOT EXISTS kudos_item_idx ON kudos (item_id);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-	err = sqlitex.Execute(
-		conn,
-		`CREATE INDEX IF NOT EXISTS kudos_creator_usernamex ON kudos (creator_username);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create sessions table.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE TABLE IF NOT EXISTS sessions (
-			token TEXT PRIMARY KEY,
-			data BLOB NOT NULL,
-			expiry REAL NOT NULL
-		);`,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create sessions index.
-	err = sqlitex.Execute(
-		conn,
-		`CREATE INDEX IF NOT EXISTS sessions_expiry_idx ON sessions(expiry);`,
-		nil,
-	)
+	err = sqlitemigration.Migrate(context.Background(), conn, schema)
 	if err != nil {
 		return nil, err
 	}
